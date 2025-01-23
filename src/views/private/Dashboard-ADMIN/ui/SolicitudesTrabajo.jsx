e1f3f88cfaece980d12615de1b2bbf6c664c7578
Washitox <washovilla78@gmail.com>
2025-01-23 00:13:00 -0500

    act boton rechazar

--- src/views/private/Dashboard-ADMIN/ui/SolicitudesTrabajo.jsx
+++ src/views/private/Dashboard-ADMIN/ui/SolicitudesTrabajo.jsx
@@ -368,14 +368,15 @@ function SolicitudesTrabajo() {
                           </button>
                         )}
                         {solicitud.cotizacionAceptada !== "COTIZACION_ACEPTADA" &&
-                          solicitud.cotizacionAceptada !== "RECHAZO_COTIZACION_USUARIO" && (
+                        solicitud.cotizacionAceptada !== "RECHAZO_COTIZACION_USUARIO" &&
+                        solicitud.estado !== "SOLICITUD_RECHAZADA" && (
                             <button
                               onClick={() => handleReject(solicitud.idSolicitud)}
                               className="w-24 py-2 text-center bg-red-500 hover:bg-red-600 text-white font-bold rounded"
                             >
                               Rechazar
                             </button>
-                          )}
+                        )}
                         <button
                           onClick={() => handleDelete(solicitud.idSolicitud)}
                           className="w-24 py-2 text-center bg-gray-500 hover:bg-gray-600 text-white font-bold rounded"
